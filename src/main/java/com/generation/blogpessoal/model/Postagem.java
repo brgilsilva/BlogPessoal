dc04616a72aaf94e5f30b307e12b3b929dd8a074
Bruno <bruno.gsilva@mercadolivre.com>
2023-01-30 12:24:38 -0300

    enviando blog Pessoal

---
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -0,0 +1,61 @@
+package com.generation.blogpessoal.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tb_postagens")
+public class Postagem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "o atributo título é obrigatório!")
+    @Size(min = 5, max = 100, message = "O atributo titulo, no minimo 5 e máximo 100!")
+    private String titulo;
+
+    @NotBlank(message = "o atributo texto é obrigatório!")
+    @Size(min = 10, max = 1000, message = "O atributo texto deve conter no minimo 10 e no maximo 1000 caracteres")
+    private String texto;
+
+    @UpdateTimestamp
+    private LocalDateTime data;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    public LocalDateTime getData() {
+        return data;
+    }
+
+    public void setData(LocalDateTime data) {
+        this.data = data;
+    }
+}
+
